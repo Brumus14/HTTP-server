c109027446e8cbae401947251d5113e6ed5b0124
Luke Barkess <luke.barkess@icloud.com>
2025-06-19 12:30:05 +0100

    Improved address creation

--- src/main.c
+++ src/main.c
@@ -3,28 +3,37 @@
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 
+struct sockaddr_in generate_address(unsigned char ip[4], unsigned short port) {
+    return (struct sockaddr_in){
+        AF_INET,
+        // Convert hardware bytes to network (little endian to big)
+        htons(port),
+        // Merge the IP bytes to form a single integer
+        {htonl(((ip[0] << 24) | (ip[1] << 16) | (ip[2] << 8) | ip[3]))},
+    };
+}
+
 int main() {
     // Create the TCP network socket
     int descriptor = socket(AF_INET, SOCK_STREAM, 0);
 
-    // The target port
-    short port = 8000;
+    if (descriptor == -1) {
+        printf("Failed to create socket");
+    }
 
+    // Form the address for the socket to connect to
     unsigned char ip[4] = {127, 0, 0, 1};
+    unsigned short port = 8000;
+    struct sockaddr_in address = generate_address(ip, port);
 
-    // The target IP address
-    struct in_addr ip_address = {
-        htonl(((ip[0] << 24) | (ip[1] << 16) | (ip[2] << 8) | ip[3]))};
+    // Attempt to connect the socket to the address
+    int connect_result =
+        connect(descriptor, (struct sockaddr *)&address, sizeof(address));
 
-    // Data for the sockets target address
-    struct sockaddr_in address = {AF_INET, htonl(port), ip_address};
-
-    // Make a connection with the socket to the address
-    if (connect(descriptor, (struct sockaddr *)&address, sizeof(address)) ==
-        -1) {
-        // Generate an error message with the ip and port
-        char message[41 + 1];
-        sprintf(message, "Error connecting to %d.%d.%d.%d:%d", ip[0], ip[1],
+    if (connect_result == -1) {
+        // Generate an error message with the IP and port
+        char message[42 + 1];
+        sprintf(message, "Failed to connect to %d.%d.%d.%d:%d", ip[0], ip[1],
                 ip[2], ip[3], port);
 
         // Output the error from the connect attempt with the generated message
