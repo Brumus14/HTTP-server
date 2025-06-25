1cad8ff55fb9d196b231334ac36a18b57f6ea0c1
Luke barkess <luke.barkess@icloud.com>
2025-06-25 11:23:07 +0100

    Starting to create the server

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
@@ -16,7 +17,7 @@ struct sockaddr_in generate_address(unsigned char ip[4], unsigned short port) {
 
 int main() {
     // Create the TCP network socket
-    int descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    int descriptor = socket(PF_INET, SOCK_STREAM, 0);
 
     if (descriptor == -1) {
         printf("Failed to create socket");
@@ -24,38 +25,19 @@ int main() {
     }
 
     // Form the address for the socket to connect to
-    // 146.190.62.39
-    unsigned char ip[4] = {146, 190, 62, 39};
-    unsigned short port = 80;
+    unsigned char ip[4] = {127, 0, 0, 1};
+    unsigned short port = 8000;
     struct sockaddr_in address = generate_address(ip, port);
 
-    // Attempt to connect the socket to the address
-    int connect_result =
-        connect(descriptor, (struct sockaddr *)&address, sizeof(address));
+    int bind_result =
+        bind(descriptor, (struct sockaddr *)&address, sizeof(address));
 
-    if (connect_result == -1) {
-        // Generate an error message with the IP and port
-        char message[42 + 1];
-        sprintf(message, "Failed to connect to %d.%d.%d.%d:%d", ip[0], ip[1],
-                ip[2], ip[3], port);
+    if (bind_result == -1) {
+        char error_message[43 + 1];
+        sprintf(error_message, "Error binding to %d.%d.%d.%d port %d", ip[0],
+                ip[1], ip[2], ip[3], port);
 
-        // Output the error from the connect attempt with the generated message
-        perror(message);
-
-        return 1;
-    }
-
-    char request[] =
-        "GET /index.html HTTP/1.1\r\nHost: www.httpforever.com\r\n\r\n";
-    send(descriptor, request, sizeof(request) - 1, 0);
-
-    char response_buffer[512 + 1];
-    int bytes_recieved = 0;
-
-    while ((bytes_recieved = recv(descriptor, response_buffer,
-                                  sizeof(response_buffer), 0)) > 0) {
-        response_buffer[bytes_recieved] = '\0';
-        printf("    %d    %s", bytes_recieved, response_buffer);
+        perror(error_message);
     }
 
     shutdown(descriptor, SHUT_RDWR);
